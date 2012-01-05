7d0275f93822435eee0dafb0bdbbfefa30eaad53
jason.weiss@nfclabs.org <jason.weiss@nfclabs.org@d051258e-10fe-9056-121f-72d4171b63b4>
2012-01-05 05:48:37 +0000

    Expanded the RI to include simple POJO classes and synchronized the model to the latest UML diagrams.

--- src/main/java/org/nfclabs/base/tagevent/INfcTagEvent.java
+++ src/main/java/org/nfclabs/base/tagevent/INfcTagEvent.java
@@ -1,29 +1,13 @@
-/*
- * Copyright 2011-2012 NFC Labs
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- */
-
 package org.nfclabs.base.tagevent;
 
 import org.nfclabs.base.IUniqueObject;
-import org.nfclabs.base.context.INfcUser;
-import org.nfclabs.base.context.INfcDevice;
 import org.nfclabs.base.context.INfcApplication;
+import org.nfclabs.base.context.INfcDevice;
 import org.nfclabs.base.context.INfcTag;
+import org.nfclabs.base.context.INfcUser;
 import org.nfclabs.base.types.INfcTagEventType;
 
-public interface INfcTagEvent extends IBitemporalObject, IUniqueObject {
+public interface INfcTagEvent extends IUniqueObject, IBitemporalObject {
  
 	public abstract INfcUser getNfcUser();
 	public abstract INfcDevice getNfcDevice();
---
+++ src/main/java/org/nfclabs/pojo/AbstractSimpleObject.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2011-2012 NFC Labs
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.nfclabs.pojo;
+
+import org.nfclabs.base.ISimpleObject;
+
+public abstract class AbstractSimpleObject implements ISimpleObject
+{
+    protected String name;
+    protected String description;
+    protected boolean activeFlag;
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return description;
+    }
+
+    @Override
+    public boolean isActive()
+    {
+        return activeFlag;
+    }
+}
